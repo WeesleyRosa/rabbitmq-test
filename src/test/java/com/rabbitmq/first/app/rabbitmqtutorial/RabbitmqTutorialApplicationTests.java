3a6cda70b3511605696f6c553d1ab89ebe689ec8
Wesley Rosa <wesley.rosa@4all.local>
2020-01-28 17:44:56 -0300

    innitial commit

---
+++ src/test/java/com/rabbitmq/first/app/rabbitmqtutorial/RabbitmqTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.rabbitmq.first.app.rabbitmqtutorial;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RabbitmqTutorialApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
